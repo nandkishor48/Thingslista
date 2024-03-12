497e9e1807da903c4a5b90285e655d5952dc1d96
nandkishor48 <nandkishorjadhao05@gmail.com>
2024-03-12 08:52:04 +0530

    doctype create

---
+++ thingslista/thingslista/doctype/route_distributors/route_distributors.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, yes and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Route Distributors", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ thingslista/thingslista/doctype/route_distributors/test_route_distributors.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, yes and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRouteDistributors(FrappeTestCase):
+	pass
